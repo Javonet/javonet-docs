a22d16b62143b3882af3b34c09ce8b1223241d05
Michał Komor <mkomor@sdncenter.pl>
2022-10-17 08:17:29 +0200

    changes in v2 python files (comments)

--- v2/snippets/python/common/activation.py
+++ v2/snippets/python/common/activation.py
@@ -3,7 +3,7 @@ from tests.ActivationCredentials import *
 
 
 def test_activation_correct_credentials_0():
-    # // <Javonet_activate>
+    // <Javonet_activate>
     result = Javonet.activate(ActivationCredentials.yourEmail, ActivationCredentials.yourLicenceKey)
-    # // </Javonet_activate>
+    // </Javonet_activate>
     assert (result == 0)
--- v2/snippets/python/nodejs/integrationTests.py
+++ v2/snippets/python/nodejs/integrationTests.py
@@ -3,79 +3,79 @@ from pathlib import Path
 
 from javonet.sdk import Javonet
 
-# // <TestResources_TestClassValues>
+// <TestResources_TestClassValues>
 javonet_src_root = str(Path(__file__).parent.parent.parent.parent)
 library_path = javonet_src_root + '/testResources/v8/V8TestClass.js'
 class_name = 'V8TestClass.js'
-# // </TestResources_TestClassValues>
+// </TestResources_TestClassValues>
 
 def test_v8_standardlibrary_invokestaticmethod_math_abs_minus50_50():
-    # // <StandardLibrary_InvokeStaticMethod>
+    // <StandardLibrary_InvokeStaticMethod>
     call = Javonet.in_memory().V8().get_type("Math").invoke_static_method("abs", -50).Execute()
     result = call.get_value()
-    # // </StandardLibrary_InvokeStaticMethod>
+    // </StandardLibrary_InvokeStaticMethod>
     assert (result == 50)
 
 
 def test_v8_standardlibrary_getstaticfield_mathpi_pi():
-    # // <StandardLibrary_GetStaticField>
+    // <StandardLibrary_GetStaticField>
     call = Javonet.in_memory().V8().get_type("Math").get_static_field("PI").Execute()
     result = call.get_value()
-    # // </StandardLibrary_GetStaticField>
+    // </StandardLibrary_GetStaticField>
     assert (round(result, 6) == round(math.pi, 6))
 
 
 def test_v8_testresources_loadlibrary_librarypath_noexception():
-    # // <TestResources_LoadLibrary>
+    // <TestResources_LoadLibrary>
     Javonet.in_memory().V8().load_library(library_path)
-    # // </TestResources_LoadLibrary>
+    // </TestResources_LoadLibrary>
 
 
 def test_v8_testresources_invokestaticmethod_multiplybytwo_25_50():
-    # // <TestResources_InvokeStaticMethod>
+    // <TestResources_InvokeStaticMethod>
     Javonet.in_memory().V8().load_library(library_path)
     call = Javonet.in_memory().V8().get_type(class_name).invoke_static_method("multiplyByTwo", 25).Execute()
     result = call.get_value()
-    # // </TestResources_InvokeStaticMethod>
+    // </TestResources_InvokeStaticMethod>
     assert (result == 50)
 
 
 def test_v8_testresources_getstaticfield_staticvalue_3():
-    # // <TestResources_GetStaticField>
+    // <TestResources_GetStaticField>
     Javonet.in_memory().V8().load_library(library_path)
     call = Javonet.in_memory().V8().get_type(class_name).get_static_field("staticValue").Execute()
     result = call.get_value()
-    # // </TestResources_GetStaticField>
+    // </TestResources_GetStaticField>
     assert (result == 3)
 
 
 def test_v8_testresources_setstaticfield_staticvalue_75():
-    # // <TestResources_SetStaticField>
+    // <TestResources_SetStaticField>
     Javonet.in_memory().V8().load_library(library_path)
     Javonet.in_memory().V8().get_type(class_name).set_static_field("staticValue", 75).Execute()
     call = Javonet.in_memory().V8().get_type(class_name).get_static_field("staticValue").Execute()
     result = call.get_value()
-    # // </TestResources_SetStaticField>
+    // </TestResources_SetStaticField>
     assert (result == 75)
     Javonet.in_memory().V8().get_type(class_name).set_static_field("staticValue", 3).Execute()
 
 
-# def test_v8_testresources_invokeinstancemethod_multiplytwonumbers_2_25_50():
-#     # // <TestResources_InvokeInstanceMethod>
-#     Javonet.in_memory().V8().load_library(library_path)
-#     instance = Javonet.in_memory().V8().get_type(class_name).create_instance(3, 4).Execute()
-#     call = instance.invoke_instance_method("multiplyTwoNumbers", 2, 25).Execute()
-#     result = call.get_value()
-#     # // </TestResources_InvokeInstanceMethod>
-#     assert (result == 50.0)
-#
-#
-# def test_v8_testresources_getinstancefield_publicvalue_3():
-#     # // <TestResources_GetInstanceField>
-#     Javonet.in_memory().V8().load_library(library_path)
-#     instance = Javonet.in_memory().V8().get_type(class_name).create_instance(3, 4).Execute()
-#     call = instance.get_instance_field("publicValue").Execute()
-#     result = call.get_value()
-#     # // </TestResources_GetInstanceField>
-#     assert (result == 3.0)
+def test_v8_testresources_invokeinstancemethod_multiplytwonumbers_2_25_50():
+    // <TestResources_InvokeInstanceMethod>
+    Javonet.in_memory().V8().load_library(library_path)
+    instance = Javonet.in_memory().V8().get_type(class_name).create_instance(3, 4).Execute()
+    call = instance.invoke_instance_method("multiplyTwoNumbers", 2, 25).Execute()
+    result = call.get_value()
+    // </TestResources_InvokeInstanceMethod>
+    assert (result == 50.0)
+
+
+def test_v8_testresources_getinstancefield_publicvalue_3():
+    // <TestResources_GetInstanceField>
+    Javonet.in_memory().V8().load_library(library_path)
+    instance = Javonet.in_memory().V8().get_type(class_name).create_instance(3, 4).Execute()
+    call = instance.get_instance_field("publicValue").Execute()
+    result = call.get_value()
+    // </TestResources_GetInstanceField>
+    assert (result == 3.0)
 
