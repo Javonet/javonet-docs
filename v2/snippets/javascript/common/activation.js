51f97dcf781bb0d83c1c2b55904cb02f629624de
Michał Komor <mkomor@sdncenter.pl>
2022-10-17 09:24:30 +0200

    v2/nodejs tests added

---
+++ v2/snippets/javascript/common/activation.js
@@ -0,0 +1,12 @@
+const {Javonet} = require('javonet/src/sdk/Javonet')
+const ActivationCredentials = require("./ActivationCredentials");
+
+describe('NodeJS activation tests', () => {
+
+    test(`Test_Activation_CorrectCredentials_Returns0`, () => {
+        // <Javonet_activate>
+        let result = Javonet.activate(ActivationCredentials.yourEmail, ActivationCredentials.yourLicenceKey)
+        // </ Javonet_activate>
+        expect(result).toBe(0)
+    })
+})
