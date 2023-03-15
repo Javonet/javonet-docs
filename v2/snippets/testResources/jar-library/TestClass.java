9329c8235998fb0191ae2ff6285e15800733a4fc
Michał Komor <mkomor@sdncenter.pl>
2023-03-15 09:31:31 +0100

    New test resources

--- v2/snippets/testResources/jar-library/TestClass.java
+++ v2/snippets/testResources/jar-library/TestClass.java
@@ -1,4 +1,9 @@
 public class TestClass {
+
+    public int publicValue;
+    private int privateValue;
+    public static int staticValue = 3;
+
     public TestClass() {
         this.publicValue = 1;
         this.privateValue = 2;
@@ -13,65 +18,7 @@ public class TestClass {
         return 2 * a;
     }
 
-    public int publicValue;
-    private int privateValue;
-    public static int staticValue = 3;
-
-
     public int multiplyTwoNumbers(int a, int b) {
         return a * b;
-    }
-
-    public int getPublicValue() {
-        return publicValue;
-    }
-
-    public void setPublicValue(int publicValue) {
-        this.publicValue = publicValue;
-    }
-
-    public int getPrivateValue() {
-        return privateValue;
-    }
-
-    public void setPrivateValue(int privateValue) {
-        this.privateValue = privateValue;
-    }
-
-    public String[] get1DArray()
-    {
-        return new String[] { "one", "two", "three", "four", "five" };
-    }
-
-    public Double[] addNumberTo1DArray(Double[] myArray, double myDouble)
-    {
-        for (int i = 0; i < myArray.length; i++)
-        {
-            myArray[i] += myDouble;
-        }
-        return myArray;
-    }
-
-    public int[][][] produce3DArray(int MyInt)
-    {
-        int[][][] responseArray =
-        {
-            {
-                {0 + MyInt, 1 + MyInt},
-                {2 + MyInt, 3 + MyInt},
-            },
-            {
-                {4 + MyInt, 5 + MyInt},
-                {6 + MyInt, 7 + MyInt}
-            }
-        };
-
-        return responseArray;
-    }
-
-    public int[][] produceJaggedArray(int[] myArray)
-    {
-        int[][] responseArray = new int[][] { myArray, myArray, myArray };
-        return responseArray;
     }
 }
--- v2/snippets/testResources/perl-package/TestClass.pm
+++ v2/snippets/testResources/perl-package/TestClass.pm
@@ -15,30 +15,6 @@ sub new {
     return bless $self, $class;
 }
 
-sub get_private_value{
-    my ($self) = @_;
-    return $self->{private_value};}
-
-sub get_public_value{
-    my ($self) = @_;
-    return $self->{public_value};}
-
-sub set_private_value{
-    my ($self, $value) = @_;
-    if (@_ == 2) {
-        $self->{private_value} = $value;
-    }
-    return $self->{private_value};
-}
-
-sub set_public_value{
-    my ($self, $value) = @_;
-    if (@_ == 2) {
-        $self->{public_value} = $value;
-    }
-    return $self->{public_value};
-}
-
 sub multiply_by_two {
     my ($first_arg) = @_;
     return $first_arg * 2;
@@ -47,18 +23,6 @@ sub multiply_by_two {
 sub multiply_two_numbers {
     my ($self, $first_arg, $second_arg) = @_;
     return $first_arg * $second_arg;
-}
-
-sub get_1d_array {
-    return ["one", "two", "three", "four", "five"];
-}
-
-sub add_number_to_1D_array {
-    my ($self, $myArray, $myDouble) = @_;
-    for(@$myArray) {
-        $_ = $_ + $myDouble;
-    }
-    return $myArray;
 }
 
 
