3318b7d5942b8ed28aad3bd9d091386524338718
Michał Komor <mkomor@sdncenter.pl>
2023-03-22 13:38:09 +0100

    cpp snippetWrapper corrected

--- v2/snippets/cpp/common/snippetWrapper.cpp
+++ v2/snippets/cpp/common/snippetWrapper.cpp
@@ -1,7 +1,9 @@
-#include "include/Javonet.h"
+#include "Javonet.h"
 #include <iostream>
 
 using namespace JavonetNS::Cpp::Sdk;
+
+std::string resourcesDirectory = ".";
 
 int main(int argc, char* argv[]) {
     //place-for-snippet
