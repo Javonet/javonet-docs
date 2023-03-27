9a5fa74bac21d33ae15e332a7bfdd0f1393191fa
Michał Komor <mkomor@sdncenter.pl>
2023-03-27 09:03:58 +0200

    SampleProgram.java updated

--- v2/snippets/java/common/SampleProgram.java
+++ v2/snippets/java/common/SampleProgram.java
@@ -1,12 +1,34 @@
+// <Import>
 import com.javonet.sdk.internal.InvocationContext;
 import com.javonet.sdk.internal.RuntimeContext;
 import com.javonet.sdk.java.Javonet;
+// </Import>
 
 public class SampleProgram {
 
     private static final String resourcesDirectory = ".";
 
     public static void main(String[] args) {
-        //place-for-snippet
+		// <WholeSnippet>
+		// <Activation>
+        Javonet.activate("your-email", "your-license-key");
+		// </Activation>
+
+		// <RuntimeContextCreation>
+        RuntimeContext pythonRuntime = Javonet.inMemory().python();
+		// </RuntimeContextCreation>
+
+		// <GetType>
+        InvocationContext pythonType = pythonRuntime.getType("math").execute();
+        // </GetType>
+
+		// <GetStaticField>
+        InvocationContext response = pythonType.getStaticField("pi").execute();
+		// </GetStaticField>
+
+		// <GetValue>
+        float result = (float) response.getValue();
+        System.out.println(result);
+		// </GetValue>
     }
 }
