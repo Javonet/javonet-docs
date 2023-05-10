36ba4e739faf9f0d85631b6434b70fe2c0a97402
Michał Komor <mkomor@sdncenter.pl>
2023-05-10 11:17:30 +0200

    new python to ruby integraiont tests

--- v2/snippets/python/ruby-package/integrationTests.py
+++ v2/snippets/python/ruby-package/integrationTests.py
@@ -6,118 +6,42 @@ from javonet.sdk import Javonet
 resources_directory = str(Path(__file__).parent.parent.parent.parent) + '/testResources/ruby-package'
 
 
-def test_ruby_standardlibrary_invokestaticmethod_math_sqrt_2500_50():
-    # <StandardLibrary_InvokeStaticMethod>
-    # use activate only once in your app
-    Javonet.activate("your-email", "your-license-key")
-
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
-
-    # get type from the runtime
-    ruby_type = ruby_runtime.get_type("Math").execute()
-
-    # invoke type's static method
-    response = ruby_type.invoke_static_method("sqrt", 2500).execute()
-
-    # get value from response
-    result = response.get_value()
-
-    # write result to console
-    print(result)
-    # </StandardLibrary_InvokeStaticMethod>
-    assert (result == 50)
-
-
-def test_ruby_standardlibrary_getstaticfield_mathpi_pi():
-    # <StandardLibrary_GetStaticField>
-    # use activate only once in your app
-    Javonet.activate("your-email", "your-license-key")
-
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
-
-    # get type from the runtime
-    ruby_type = ruby_runtime.get_type("Math").execute()
-
-    # get type's static field
-    response = ruby_type.get_static_field("PI").execute()
-
-    # get value from response
-    result = response.get_value()
-
-    # write result to console
-    print(result)
-    # </StandardLibrary_GetStaticField>
-    assert (round(result, 6) == round(math.pi, 6))
-
-
-def test_ruby_testresources_loadlibrary_librarypath_noexception():
+def test_ruby_TestResources_LoadLibrary():
     # <TestResources_LoadLibrary>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
     # </TestResources_LoadLibrary>
 
 
-def test_ruby_testresources_invokestaticmethod_multiplybytwo_25_50():
-    # <TestResources_InvokeStaticMethod>
-    # use activate only once in your app
-    Javonet.activate("your-email", "your-license-key")
-
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
-
-    # set up variables
-    library_path = resources_directory + '/TestClass.rb'
-    class_name = "TestClass::TestClass"
-
-    # load custom library
-    ruby_runtime.load_library(library_path)
-
-    # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
-
-    # invoke type's static method
-    response = ruby_type.invoke_static_method("multiply_by_two", 25).execute()
-
-    # get value from response
-    result = response.get_value()
-
-    # write result to console
-    print(result)
-    # </TestResources_InvokeStaticMethod>
-    assert (result == 50)
-
-
-def test_ruby_testresources_getstaticfield_staticvalue_3():
+def test_ruby_TestResources_GetStaticField():
     # <TestResources_GetStaticField>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # get type's static field
-    response = ruby_type.get_static_field("static_value").execute()
+    response = called_runtime_type.get_static_field("static_value").execute()
 
     # get value from response
     result = response.get_value()
@@ -128,29 +52,29 @@ def test_ruby_testresources_getstaticfield_staticvalue_3():
     assert (result == 3)
 
 
-def test_ruby_testresources_setstaticfield_staticvalue_75():
+def test_ruby_TestResources_SetStaticField():
     # <TestResources_SetStaticField>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # set static field value
-    ruby_type.set_static_field("static_value", 75).execute()
+    called_runtime_type.set_static_field("static_value", 75).execute()
 
     # get type's static field
-    response = ruby_type.get_static_field("static_value").execute()
+    response = called_runtime_type.get_static_field("static_value").execute()
 
     # get value from response
     result = response.get_value()
@@ -158,31 +82,92 @@ def test_ruby_testresources_setstaticfield_staticvalue_75():
     # write result to console
     print(result)
     # </TestResources_SetStaticField>
-    ruby_type.set_static_field("static_value", 3).execute()
+    called_runtime_type.set_static_field("static_value", 3).execute()
     assert (result == 75)
 
 
-
-def test_ruby_testresources_invokeinstancemethod_multiplytwonumbers_2_25_50():
-    # <TestResources_InvokeInstanceMethod>
+def test_ruby_TestResources_GetInstanceField():
+    # <TestResources_GetInstanceField>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(13, 14).execute()
+    instance = called_runtime_type.create_instance(18, 19).execute()
+
+    # get instance's field
+    response = instance.get_instance_field("public_value").execute()
+
+    # get value from response
+    result = response.get_value()
+
+    # write result to console
+    print(result)
+    # </TestResources_GetInstanceField>
+    assert (result == 18)
+
+def test_ruby_TestResources_InvokeStaticMethod():
+    # <TestResources_InvokeStaticMethod>
+    # use activate only once in your app
+    Javonet.activate("your-email", "your-license-key")
+
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
+
+    # set up variables
+    library_path = resources_directory + '/TestClass.rb'
+    class_name = "TestClass::TestClass"
+
+    # load custom library
+    called_runtime.load_library(library_path)
+
+    # get type from the runtime
+    called_runtime_type = called_runtime.get_type(class_name).execute()
+
+    # invoke type's static method
+    response = called_runtime_type.invoke_static_method("multiply_by_two", 25).execute()
+
+    # get value from response
+    result = response.get_value()
+
+    # write result to console
+    print(result)
+    # </TestResources_InvokeStaticMethod>
+    assert (result == 50)
+
+
+def test_ruby_TestResources_InvokeInstanceMethod():
+    # <TestResources_InvokeInstanceMethod>
+    # use activate only once in your app
+    Javonet.activate("your-email", "your-license-key")
+
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
+
+    # set up variables
+    library_path = resources_directory + '/TestClass.rb'
+    class_name = "TestClass::TestClass"
+
+    # load custom library
+    called_runtime.load_library(library_path)
+
+    # get type from the runtime
+    called_runtime_type = called_runtime.get_type(class_name).execute()
+
+    # create type's instance
+    instance = called_runtime_type.create_instance(13, 14).execute()
 
     # invoke instance's method
     response = instance.invoke_instance_method("multiply_two_numbers", 2, 25).execute()
@@ -196,59 +181,26 @@ def test_ruby_testresources_invokeinstancemethod_multiplytwonumbers_2_25_50():
     assert (result == 50)
 
 
-def test_ruby_testresources_getinstancefield_publicvalue_18():
-    # <TestResources_GetInstanceField>
-    # use activate only once in your app
-    Javonet.activate("your-email", "your-license-key")
-
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
-
-    # set up variables
-    library_path = resources_directory + '/TestClass.rb'
-    class_name = "TestClass::TestClass"
-
-    # load custom library
-    ruby_runtime.load_library(library_path)
-
-    # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
-
-    # create type's instance
-    instance = ruby_type.create_instance(18, 19).execute()
-
-    # get instance's field
-    response = instance.get_instance_field("public_value").execute()
-
-    # get value from response
-    result = response.get_value()
-
-    # write result to console
-    print(result)
-    # </TestResources_GetInstanceField>
-    assert (result == 18)
-
-
-def test_ruby_testresources_1darray_get_index_2_string_three():
+def test_ruby_TestResources_1DArray_GetIndex():
     # <TestResources_1DArray_GetIndex>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -265,26 +217,26 @@ def test_ruby_testresources_1darray_get_index_2_string_three():
     assert (result == "three")
 
 
-def test_ruby_testresources_1darray_get_size_5():
+def test_ruby_TestResources_1DArray_GetSize():
     # <TestResources_1DArray_GetSize>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -301,26 +253,26 @@ def test_ruby_testresources_1darray_get_size_5():
     assert (result == 5)
 
 
-def test_ruby_testresources_1darray_set_index_string_seven():
+def test_ruby_TestResources_1DArray_SetIndex():
     # <TestResources_1DArray_SetIndex>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -341,27 +293,26 @@ def test_ruby_testresources_1darray_set_index_string_seven():
     assert (result == "seven")
 
 
-
-def test_ruby_testresources_1darray_iterate_strings():
+def test_ruby_TestResources_1DArray_Iterate():
     # <TestResources_1DArray_Iterate>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -377,26 +328,26 @@ def test_ruby_testresources_1darray_iterate_strings():
     assert array_upper == ["ONE", "TWO", "THREE", "FOUR", "FIVE"]
 
 
-def test_jvm_testresources_1darray_get_element_string():
+def test_ruby_TestResources_1DArray_GetElement():
     # <TestResources_1DArray_GetElement>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -413,26 +364,26 @@ def test_jvm_testresources_1darray_get_element_string():
     assert result == "THREE"
 
 
-def test_jvm_testresources_1darray_set_element_string():
+def test_ruby_TestResources_1DArray_SetElement():
     # <TestResources_1DArray_SetElement>
     # use activate only once in your app
     Javonet.activate("your-email", "your-license-key")
 
-    #    # create ruby runtime context
-    ruby_runtime = Javonet.in_memory().ruby()
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
 
     # set up variables
     library_path = resources_directory + '/TestClass.rb'
     class_name = "TestClass::TestClass"
 
     # load custom library
-    ruby_runtime.load_library(library_path)
+    called_runtime.load_library(library_path)
 
     # get type from the runtime
-    ruby_type = ruby_runtime.get_type(class_name).execute()
+    called_runtime_type = called_runtime.get_type(class_name).execute()
 
     # create type's instance
-    instance = ruby_type.create_instance(0, 1).execute()
+    instance = called_runtime_type.create_instance(0, 1).execute()
 
     # invoke instance's method
     array = instance.invoke_instance_method("get_1d_array").execute()
@@ -450,3 +401,52 @@ def test_jvm_testresources_1darray_set_element_string():
     print(result)
     # </TestResources_1DArray_SetElement>
     assert result == "ZERO"
+
+
+def test_ruby_StandardLibrary_GetStaticField():
+    # <StandardLibrary_GetStaticField>
+    # use activate only once in your app
+    Javonet.activate("your-email", "your-license-key")
+
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
+
+    # get type from the runtime
+    called_runtime_type = called_runtime.get_type("Math").execute()
+
+    # get type's static field
+    response = called_runtime_type.get_static_field("PI").execute()
+
+    # get value from response
+    result = response.get_value()
+
+    # write result to console
+    print(result)
+    # </StandardLibrary_GetStaticField>
+    assert (round(result, 6) == round(math.pi, 6))
+
+
+def test_ruby_StandardLibrary_InvokeStaticMethod():
+    # <StandardLibrary_InvokeStaticMethod>
+    # use activate only once in your app
+    Javonet.activate("your-email", "your-license-key")
+
+    # create called runtime context
+    called_runtime = Javonet.in_memory().ruby()
+
+    # get type from the runtime
+    called_runtime_type = called_runtime.get_type("Math").execute()
+
+    # invoke type's static method
+    response = called_runtime_type.invoke_static_method("sqrt", 2500).execute()
+
+    # get value from response
+    result = response.get_value()
+
+    # write result to console
+    print(result)
+    # </StandardLibrary_InvokeStaticMethod>
+    assert (result == 50)
+
+
+
