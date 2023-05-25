f41da7e8b6764275fcd391007381219477a86260
Michał Komor <mkomor@sdncenter.pl>
2023-05-25 13:16:24 +0200

    fluent interface in progress

--- v1/getting-started/fluent-interface.md
+++ v1/getting-started/fluent-interface.md
@@ -5,6 +5,10 @@
 
 # Using the Javonet Fluent Interface
 
-Javonet offers a robust fluent interface that lets you simplify and shorten operations on .NET objects.
+Javonet offers a robust fluent interface that lets you simplify and shorten operations on objects.
 
-:::code source="~/v1/snippets/`{calling_technology}`/`{called_technology}`/fluent-interface.`{calling_ext}`":::
+:::code 
+:calling_source: v1/snippets/`{calling_technology}`/`{called_technology}`/sampleProgram.`{calling_ext}`
+:calling_id: FluentInterface
+:display: calling
+:::
--- v1/snippets/java/netframework-dll/sampleProgram.java
+++ v1/snippets/java/netframework-dll/sampleProgram.java
@@ -15,5 +15,24 @@ class SampleProgram {
     Javonet.addReference("System.Windows.Forms","System.Drawing");
     Javonet.addReference("TestClass.dll");
     // </AddReference>
+
+    // <FluentInterface>
+    //Regular code
+    NObject nowDateObj= Javonet.getType("DateTime").get("Now");
+        
+    NObject date = Javonet.New("DateTime",1980,1,1);
+    NObject datesDiff = nowDateObj.invoke("Subtract",date);
+
+    String dateDiffStr = datesDiff.invoke("ToString");
+
+    System.out.println(dateDiffStr); 
+
+    //Fluent code
+    String dateDiffStr = Javonet.getType("DateTime")
+        .<NObject>get("Now")
+        .<NObject>invoke("Subtract",Javonet.New("DateTime",1980,1,1))
+        .invoke("ToString");
+    System.out.println(dateDiffStr);
+    // </FluentInterface>
     }
 }
