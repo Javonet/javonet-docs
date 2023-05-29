664eff0d9d6f2cfff9fd29210225f6f172ad4307
Michał Komor <mkomor@sdncenter.pl>
2023-05-29 06:36:25 +0200

    TestClasses updated

--- v1/snippets/jar-library/TestClass.java
+++ v1/snippets/jar-library/TestClass.java
@@ -1,12 +1,29 @@
 public class TestClass {
-    public TestClass() {}
+    public TestClass() {
+    }
+
+    public static int MyStaticField;
+
+    public int MyInstanceField;
+
     public static String SayHello(String name) {
         return "Hello " + name;
     }
+
     public static int MethodExpectingPrimitiveInt(int arg) {
-        return arg*2;
+        return arg * 2;
     }
+
     public static int MethodExpectingClassInteger(Integer arg) {
-        return arg*2;
+        return arg * 2;
+    }
+
+    public int MultiplyByTwo(Integer arg) {
+        return arg * 2;
+    }
+
+    public <T> T MyGenericMethod(T arg1)
+    {
+        return arg1;
     }
 }
--- v1/snippets/netframework-dll/TestClass.cs
+++ v1/snippets/netframework-dll/TestClass.cs
@@ -4,9 +4,18 @@ namespace TestClass
 	{
 		public TestClass() { }
 
+		public static int MyStaticField {get;set; }
+
+		public int MyInstanceField { get;set; }
+
 		public static string SayHello(string name)
 		{
 			return "Hello " + name;
+		}
+
+		public int MultiplyByTwo(int arg)
+		{
+			return arg * 2;
 		}
 
 		public T MyGenericMethod<T>(T arg1)
@@ -17,6 +26,5 @@ namespace TestClass
 		{
 			return default(K);
 		}
-
 	}
 }
