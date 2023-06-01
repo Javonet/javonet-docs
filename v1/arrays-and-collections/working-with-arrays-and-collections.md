4383f49d7ce39d9ad75a7ff32d4b0ebce7ccd7eb
Michał Komor <mkomor@sdncenter.pl>
2023-06-01 14:46:36 +0200

    working-with-arrays-and-collections-in-progress

---
+++ v1/arrays-and-collections/working-with-arrays-and-collections.md
@@ -0,0 +1,39 @@
+:::options
+:title: Using Value-Type and Reference-Type Arrays
+:description: Using Value-Type and Reference-Type Arrays
+:related_articles: arrays-and-collections/using-value-type-and-reference-type-arrays.md
+:::
+  
+# Working with arrays and collections  
+
+Data structures are one of the essential aspects of every piece of software that has ever been written. Any
+application, from simple command line util to scalable enterprise systems, constantly process various
+information, that very often require specific grouping and access strategies. This aspect is addressed by
+arrays and more advanced collection types available both in Java and .NET environments. By using the
+Javonet framework, users gain ability to easily and effectively work with data structures originating
+from the other platform.  
+  
+## Working with Arrays
+
+During the array part of this tutorial, I'll be working with the following pseudo-service class, which main
+purpose is to produce and consume various array-typed objects.
+
+:::code 
+:called_source: v1/snippets/`{called_technology}`/ArrayService.`{called_ext}`
+:display: called
+:::
+  
+
+
+
+
+
+
+## Summary  
+  
+In this article we've focused on working with arrays and collections using Javonet
+framework. As for array objects, differences of handling arrays of value- and reference-type objects have
+been discussed, as well as framework's current limitations have been presented. We've briefly explained
+basic techniques of handling collection objects, covering retrieval, enumeration- and index-based
+access to their contents, as well as instantiation and usage as arguments in the method calls.
+  
---
+++ v1/snippets/netframework-dll/ArrayService.cs
@@ -0,0 +1,78 @@
+namespace TestNamespace
+{
+	class ArrayService
+	{
+
+		public int[] GetIntArray()
+		{
+			return new int[] { 1, 2, 3, 4 };
+		}
+
+		public void UseIntArray(int[] intArray)
+		{
+			System.Console.WriteLine("[.NET] Contents of primitive-type array:");
+
+			foreach (int intVal in intArray)
+			{
+				System.Console.WriteLine("{0} ", intVal);
+			}
+		}
+
+		public int[][] GetArrayOfIntArrays()
+		{
+			int[][] arrayOfIntArrays = new int[2][];
+
+			arrayOfIntArrays[0] = new int[4] { 1, 2, 3, 4 };
+			arrayOfIntArrays[1] = new int[4] { 11, 22, 33, 44 };
+
+			return arrayOfIntArrays;
+		}
+
+		public CustomObject[] GetCustomObjectArray()
+		{
+			return new CustomObject[]{ new CustomObject("A",1),
+	  new CustomObject("B", 2),
+	  new CustomObject("C", 3)};
+		}
+
+		public void UseCustomObjectArray(CustomObject[] cuArray)
+		{
+			System.Console.WriteLine("[.NET] Contents of the reference-type array:");
+
+			foreach (CustomObject cu in cuArray)
+			{
+				System.Console.WriteLine("{0}[{1}] ", cu.Name, cu.Value);
+			}
+		}
+
+		public CustomObject[][] GetArrayOfCustomObjectArrays()
+		{
+			CustomObject[][] arrayOfCustomObjectArrays = new CustomObject[2][];
+
+			arrayOfCustomObjectArrays[0] = new CustomObject[]{
+	  new CustomObject("A1",1),
+	  new CustomObject("A2", 2),
+	  new CustomObject("A3", 3)};
+
+			arrayOfCustomObjectArrays[1] = new CustomObject[]{
+	  new CustomObject("B1",1),
+	  new CustomObject("B2", 2),
+	  new CustomObject("B3", 3)};
+
+			return arrayOfCustomObjectArrays;
+		}
+	}
+
+	class CustomObject
+	{
+
+		public string Name { get; set; }
+		public int Value { get; set; }
+
+		public CustomObject(string name, int value)
+		{
+			this.Name = name;
+			this.Value = value;
+		}
+	}
+}
